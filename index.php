434c091086174f89e5999cfeb0f029b5c01e7370
CusQuisKas <cusquiskas@gmail.com>
2021-07-15 23:56:25 +0200

    añadido el framework de HTML y JS que se usara de base para cualquier proyecto inicial y con esto tengo montado un arquetipo basico para empezar a implementar cualquier nueva aplicacion

--- index.php
+++ index.php
@@ -1,70 +1,31 @@
 <?php
     session_start();
-    error_reporting(E_ALL & ~E_NOTICE);
-
-    require_once 'conex/conf.php';  //información crítica del sistema
-    require_once 'conex/dao.php';   //control de comunicación con la base de datos MySQL
-
-    require_once 'tabla/controller.php';   //genera la clase de una tabla dinámicamente bajo petición
-
-    header('Content-Type: application/json; charset=utf-8');
-
-    /*$link = new ConexionSistema();
-    $datos = $link->consulta("select REFERENCED_TABLE_NAME as tabla, REFERENCED_COLUMN_NAME as columna
-    from information_schema.key_column_usage
-   where table_name = 'ARTICULO'
-     and referenced_table_name <> ''", []);
-    if ($link->hayError()) {
-        $link->close();
-        die(json_encode(['success' => false, 'root' => $manejador->getListaErrores()]));
-    }
-    $link->close();
-    unset($link);
-    die(json_encode($datos));
-    */
-
-    $manejador = ControladorDinamicoTabla::set('ARTICULO');
-    /*if ($manejador->delete(['art_codart' => 4]) > 0) {
-        die(json_encode(['success' => false, 'root' => $manejador->getListaErrores()]));
-    }*/
-
-    if ($manejador->give([]) != 0) {
-        die(json_encode(['success' => false, 'root' => $manejador->getListaErrores()]));
-    }
-    //die(json_encode($manejador->getListaErrores()));
-
-    $listaArticulo = $manejador->getArray();
-
-    //die(json_encode($listaArticulo));
-
-    /*$manejador = ControladorDinamicoTabla::set('ESPECIFICACION');
-    $manFamilia = ControladorDinamicoTabla::set('FAMILIA');
-    $manIVA = ControladorDinamicoTabla::set('IVA');
-    $i = -1;
-    foreach ($listaArticulo as $valor) {
-        ++$i;
-        if ($manejador->give(['esp_codart' => $valor['art_codart']]) != 0) {
-            die(json_encode($manejador->getListaErrores()));
-        } else {
-            $listaArticulo[$i]['especificacion'] = $manejador->getArray();
-        }
-        if ($manFamilia->give(['fam_codfam' => $valor['art_codfam']]) != 0) {
-            die(json_encode($manFamilia->getListaErrores()));
-        } else {
-            $listaArticulo[$i]['familia'] = $manFamilia->getArray();
-            if ($manIVA->give(['iva_codiva' => $listaArticulo[$i]['familia'][0]['fam_codiva']]) != 0) {
-                die(json_encode($manIVA->getListaErrores()));
-            } else {
-                $listaArticulo[$i]['familia'][0]['IVA'] = $manIVA->getArray();
-            }
-        }
-    }*/
-
-    echo json_encode(['success' => true, 'root' => $listaArticulo]);
-
-    unset($manejador);
-    unset($manFamilia);
-    unset($manIVA);
-
+    require_once 'js/function.php';
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Arquetipo para nuevos proyectos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon"       href="css/img/imagen.gif" type="image/png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo Cache::ruta('css/style.css'); ?>">
+</head>
+<body>
+    <template id="header"></template>
+    <template id="body"></template>
+    <template id="footer"></template>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script>let session = "<?php echo session_id(); ?>";</script>
+    <script src="<?php echo Cache::ruta('js/funciones.js'); ?>"></script>
+    <script src="<?php echo Cache::ruta('js/peticionAjax.js'); ?>"></script>
+    <script src="<?php echo Cache::ruta('js/controller.js'); ?>"></script>
+    <script src="<?php echo Cache::ruta('js/scripts.js'); ?>"></script>
+</body>
+</html>
